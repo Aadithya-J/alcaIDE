ddbe9a1b5a14cb207acfb497c83b5dd36cd51906
Aadithya-J <jlaadithya@gmail.com>
2025-04-25 18:18:19 +0530

    restructured code

--- cmd/main.go
+++ cmd/main.go
@@ -3,35 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
-	"context"
 	"github.com/Aadithya-J/alcaIDE/internal/router"
-	"github.com/jackc/pgx/v5"
-	"os"
-	"github.com/joho/godotenv"
+	"github.com/Aadithya-J/alcaIDE/internal/config"
+	"github.com/Aadithya-J/alcaIDE/internal/db"
 )
 
 func main() {
-	//.env
-	err := godotenv.Load()
-    if err != nil {
-        log.Println("Error loading .env file")
-    }
+	config.LoadEnv();
 
-	//db
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
-	log.Println("Connected toPostgres  Database!")
+	db.Initialize();
+	defer db.Close()
 
-
-	//api
 	mux := router.Setup()
 
 	log.Println("Server is running on :8080")
-	err = http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ internal/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+    "context"
+    "log"
+    "os"
+    "github.com/jackc/pgx/v5"
+)
+
+var Conn *pgx.Conn
+
+func Initialize() {
+    var err error
+    Conn, err = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatalf("Unable to connect to database: %v", err)
+        os.Exit(1)
+    }
+    log.Println("Connected to Postgres Database!")
+}
+
+func Close() {
+    if Conn != nil {
+        Conn.Close(context.Background())
+        log.Println("Database connection closed.")
+    }
+}
